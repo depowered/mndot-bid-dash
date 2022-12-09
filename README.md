85fcfd2f389e074460a2b9ba52b5086db23c2feb
depowered <dpwr16@gmail.com>
2022-12-09 19:42:57 +0000

    configure app for serving with gunicorn

---
+++ README.md
@@ -0,0 +1,4 @@
+# Serve Application
+```
+$ gunicorn --workers=4 --bind=127.0.0.1:8050 main:server
+```
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import dash_bootstrap_components as dbc
 from dash import Dash
+from flask import Flask
 
 from src.components.layout import create_layout
 from src.data.bid_data import BidDataFactory
@@ -7,18 +8,20 @@ from src.data.contract_data import ContractData
 from src.data.item_data import ItemData
 
 
-def main() -> None:
+def configure_app() -> tuple[Dash, Flask]:
 
     item_data = ItemData()
     contract_data = ContractData()
     bid_data_factory = BidDataFactory(item_data, contract_data)
 
-    app = Dash(external_stylesheets=[dbc.themes.DARKLY])
+    app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
     app.title = "MnDOT Bid Prices Dashboard"
     app.layout = create_layout(app, item_data, bid_data_factory)
-    # app.run()
-    app.run(dev_tools_hot_reload=True)
 
+    return (app, app.server)
+
+
+app, server = configure_app()
 
 if __name__ == "__main__":
-    main()
+    app.run()
